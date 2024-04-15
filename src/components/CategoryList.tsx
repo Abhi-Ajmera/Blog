32d7a97b8729d9faf40747120f680becc7be52dc
Abhishek Ajmera <abhishekajmera17@gmail.com>
2024-04-15 12:53:22 +0530

    categories component added and make featured component responsive

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,4 +1,91 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const CategoryList = () => {
-	return <div>CategoryList</div>;
+	return (
+		<div>
+			<h1 className="my-12">Popular Caregories</h1>
+			<div className="flex flex-wrap justify-between gap-5">
+				<Link
+					href={"/blog?cat=style"}
+					className="flex items-center gap-3 capitalize h-20 justify-center rounded-lg text-softBgDark bg-blue-100 w-[105%] sm:w-[45%] md:w-[30%] lg:w-[14%]"
+				>
+					<Image
+						src="/style.png"
+						alt=""
+						width={32}
+						height={32}
+						className="rounded-[50%] h-8"
+					/>
+					<span>style</span>
+				</Link>
+				<Link
+					href={"/blog?cat=fashion"}
+					className="flex items-center gap-3 capitalize h-20 justify-center rounded-lg text-softBgDark bg-pink-100 w-[105%] sm:w-[45%] md:w-[30%] lg:w-[14%]"
+				>
+					<Image
+						src="/fashion.png"
+						alt=""
+						width={32}
+						height={32}
+						className="rounded-[50%] h-8"
+					/>
+					<span>fashion</span>
+				</Link>
+				<Link
+					href={"/blog?cat=food"}
+					className="flex items-center gap-3 capitalize h-20 justify-center rounded-lg text-softBgDark bg-green-100 w-[105%] sm:w-[45%] md:w-[30%] lg:w-[14%]"
+				>
+					<Image
+						src="/food.png"
+						alt=""
+						width={32}
+						height={32}
+						className="rounded-[50%] h-8"
+					/>
+					<span>food</span>
+				</Link>
+				<Link
+					href={"/blog?cat=travel"}
+					className="flex items-center gap-3 capitalize h-20 justify-center rounded-lg text-softBgDark bg-red-100 w-[105%] sm:w-[45%] md:w-[30%] lg:w-[14%]"
+				>
+					<Image
+						src="/travel.png"
+						alt=""
+						width={32}
+						height={32}
+						className="rounded-[50%] h-8"
+					/>
+					<span>travel</span>
+				</Link>
+				<Link
+					href={"/blog?cat=culture"}
+					className="flex items-center gap-3 capitalize h-20 justify-center rounded-lg text-softBgDark bg-yellow-100 w-[105%] sm:w-[45%] md:w-[30%] lg:w-[14%]"
+				>
+					<Image
+						src="/culture.png"
+						alt=""
+						width={32}
+						height={32}
+						className="rounded-[50%] h-8"
+					/>
+					<span>culture</span>
+				</Link>
+				<Link
+					href={"/blog?cat=coding"}
+					className="flex items-center gap-3 capitalize h-20 justify-center rounded-lg text-softBgDark bg-purple-100 w-[105%] sm:w-[45%] md:w-[30%] lg:w-[14%]"
+				>
+					<Image
+						src="/coding.png"
+						alt=""
+						width={32}
+						height={32}
+						className="rounded-[50%] h-8"
+					/>
+					<span>coding</span>
+				</Link>
+			</div>
+		</div>
+	);
 };
 export default CategoryList;
--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -4,19 +4,19 @@ const Featured = () => {
 	return (
 		<div className="mt-6">
 			{/*title  */}
-			<h1 className="text-6xl text-center">Hey, Discover my stories and creative ideas.</h1>
+			<h1 className="text-4xl md:text-5xl lg:text-6xl text-center">Hey, Discover my stories and creative ideas.</h1>
 			{/* post */}
-			<div className="mt-16 flex items-center gap-12">
-				<div className="flex-1 relative h-[500px]">
+			<div className="mt-12 md:mt-16 flex items-center gap-12">
+				<div className="hidden md:block flex-1 relative h-[500px]">
 					<Image
 						src={"/p1.jpeg"}
 						alt=""
 						layout="fill"
-						objectFit="cover"
+						className=" object-cover"
 					/>
 				</div>
 				<div className="flex-1 flex flex-col gap-5">
-					<h1 className="text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+					<h1 className="text-2xl md:text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
 					<p className="text-xl font-light text-softText dark:text-softTextDark">
 						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam porro tempora facilis laborum voluptates
 						ex nulla, recusandae ullam deleniti, voluptate sed voluptatum
